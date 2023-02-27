23bad115eda2edd84db15496b612a53dbb608b07
B20DCPT094 <wuochuy02@gmail.com>
2023-02-27 22:12:37 +0700

    update

--- script.js
+++ script.js
@@ -15,37 +15,51 @@ const scores = [0, 0];
 score0El.textContent = 0;
 score1El.textContent = 0;
 let currentPlayer = 0;
+let ok = true;
 
 let currentScore = 0;
 
 diceEl.classList.add('hidden');
 
-btnRollEl.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.src = `dice-${dice}.png`;
-  diceEl.classList.remove('hidden');
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-  } else {
-    currentScore = 0;
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    currentPlayer0El.classList.toggle('player--active');
-    currentPlayer1El.classList.toggle('player--active');
-  }
-});
-
-btnHoldEl.addEventListener('click', function () {
-  scores[currentPlayer] += currentScore;
-  document.querySelector(`#score--${currentPlayer}`).textContent =
-    scores[currentPlayer];
+const switchPlayer = function () {
   currentScore = 0;
   document.getElementById(`current--${currentPlayer}`).textContent =
     currentScore;
   currentPlayer = currentPlayer === 0 ? 1 : 0;
   currentPlayer0El.classList.toggle('player--active');
   currentPlayer1El.classList.toggle('player--active');
+};
+
+btnRollEl.addEventListener('click', function () {
+  if (ok) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHoldEl.addEventListener('click', function () {
+  if (ok) {
+    scores[currentPlayer] += currentScore;
+    document.querySelector(`#score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+    if (scores[currentPlayer] >= 20) {
+      ok = false;
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
 });
